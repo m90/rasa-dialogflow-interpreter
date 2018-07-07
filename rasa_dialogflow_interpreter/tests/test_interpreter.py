a56b331b5947cdbf01c007f7081eaa4c49073c4f
Frederik Ring <frederik.ring@gmail.com>
2018-07-07 16:26:23 +0200

    add more tests for class instance

--- rasa_dialogflow_interpreter/tests/test_interpreter.py
+++ rasa_dialogflow_interpreter/tests/test_interpreter.py
@@ -1,6 +1,9 @@
 import unittest
 
-from rasa_dialogflow_interpreter.interpreter import build_response
+import dialogflow
+
+from rasa_dialogflow_interpreter.interpreter import build_response, DialogflowInterpreter
+
 
 class TestBuildResponse(unittest.TestCase):
     def test_default(self):
@@ -35,3 +38,45 @@ class TestBuildResponse(unittest.TestCase):
                 'confidence': 1.0,
             })
         self.assertDictEqual(expected, result)
+
+
+class MockSessionsClient:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def session_path(self, project_id, session_id):
+        return '{}-{}'.format(project_id, session_id)
+
+    def detect_intent(self, session=None, query_input=None):
+        return {
+            'query_result': {
+                'query_text': 'Hello human',
+                'intent': {
+                    'display_name': 'welcome',
+                },
+                'intent_detection_confidence': 0.85,
+            },
+        }
+
+
+class TestInterpreter(unittest.TestCase):
+    def setUp(self):
+        self._sessions_client = dialogflow.SessionsClient
+        dialogflow.SessionsClient = MockSessionsClient
+
+    def tearDown(self):
+        dialogflow.SessionsClient = self._sessions_client
+
+    def test_default_properties(self):
+        interpreter = DialogflowInterpreter(
+            project_id='my-dflow-projjy', language_code='es')
+
+        self.assertEqual(interpreter.project_id, 'my-dflow-projjy')
+        self.assertEqual(interpreter.language_code, 'es')
+
+    def test_language_code_property(self):
+        interpreter = DialogflowInterpreter(
+            project_id='my-dflow-projjy')
+
+        self.assertEqual(interpreter.project_id, 'my-dflow-projjy')
+        self.assertEqual(interpreter.language_code, 'en')
