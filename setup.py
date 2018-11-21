86c9e580812d6b2ff0f95321406d60c721550149
Frederik Ring <frederik.ring@gmail.com>
2018-11-21 20:43:34 +0100

    support rasa_core~=0.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rasa_dialogflow_interpreter",
-    version="0.2.0",
+    version="0.3.0",
     author="Frederik Ring",
     author_email="frederik.ring@gmail.com",
     description="rasa_core interpreter connecting to dialogflow.com API v2",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/m90/rasa-dialogflow-interpreter",
     packages=["rasa_dialogflow_interpreter"],
-    install_requires=["dialogflow~=0.4", "rasa-core~=0.11"],
+    install_requires=["dialogflow~=0.4", "rasa-core~=0.12"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
